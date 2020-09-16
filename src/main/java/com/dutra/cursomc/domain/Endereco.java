04aa8cb63714a2cb1f813ee3fc062729953eb22d
Rafael Dutra <dutrarafaeloliveira@gmail.com>
2020-09-16 17:00:29 -0300

    Cria endpoint /clientes/{id}

--- src/main/java/com/dutra/cursomc/domain/Endereco.java
+++ src/main/java/com/dutra/cursomc/domain/Endereco.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Endereco implements Serializable {
 	
@@ -34,6 +36,7 @@ public class Endereco implements Serializable {
 	@Column
 	private String cep;
 	
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
