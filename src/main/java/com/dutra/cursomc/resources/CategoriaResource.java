24155954c86579252c948426f418f4ee0199dc6e
Rafael Dutra <dutrarafaeloliveira@gmail.com>
2020-09-20 16:13:58 -0300

    Cria endpoint /categorias/page GET

--- src/main/java/com/dutra/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/dutra/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -56,6 +58,17 @@ public class CategoriaResource {
 		List<Categoria> categorias = service.findAll();
 		List<CategoriaDTO> categoriasDTO = categorias.stream().map(obj -> new CategoriaDTO(obj))
 				.collect(Collectors.toList());
+		return ResponseEntity.ok().body(categoriasDTO);
+	}
+	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "pagina", defaultValue = "0") Integer pagina,
+			@RequestParam(value = "linhasPorPagina", defaultValue = "24") Integer linhasPorPagina,
+			@RequestParam(value = "ordenarPor", defaultValue = "nome") String ordenaPor,
+			@RequestParam(value = "Direcao", defaultValue = "ASC") String direcao) {
+		Page<Categoria> categorias = service.findPage(pagina, linhasPorPagina, ordenaPor, direcao);
+		Page<CategoriaDTO> categoriasDTO = categorias.map(obj -> new CategoriaDTO(obj)); 
 		return ResponseEntity.ok().body(categoriasDTO);
 	}
 
--- src/main/java/com/dutra/cursomc/services/CategoriaService.java
+++ src/main/java/com/dutra/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.dutra.cursomc.domain.Categoria;
@@ -45,6 +48,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return repository.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer pagina, Integer linhasPorPagina, String ordenaPor, String direcao) {
+		PageRequest pageRequest = PageRequest.of(pagina, linhasPorPagina, Direction.valueOf(direcao), ordenaPor);
+		return repository.findAll(pageRequest);
 	}
 
 }
