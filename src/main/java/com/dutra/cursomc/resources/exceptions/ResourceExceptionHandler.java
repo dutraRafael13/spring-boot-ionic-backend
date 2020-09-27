8bf59e301769383fdf0e336e6153620b12c66098
Rafael Dutra <dutrarafaeloliveira@gmail.com>
2020-09-27 12:37:26 -0300

    Trata erro validacao

--- src/main/java/com/dutra/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/dutra/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,17 +15,29 @@ import com.dutra.cursomc.services.exceptions.ObjectNotFoundException;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
+	private static final String ERRO_DE_VALIDAÇÃO = "Erro de validação";
+
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(),
 				System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
 	}
-	
+
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
 				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(error);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		ValidationError error = new ValidationError(HttpStatus.BAD_REQUEST.value(), ERRO_DE_VALIDAÇÃO,
+				System.currentTimeMillis());
+		e.getBindingResult().getFieldErrors().forEach(fieldError -> {
+			error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+		});
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(error);
 	}
 
---
+++ src/main/java/com/dutra/cursomc/resources/exceptions/ValidationError.java
@@ -0,0 +1,25 @@
+package com.dutra.cursomc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> erros;
+
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+		this.erros = new ArrayList<>();
+	}
+
+	public List<FieldMessage> getErrors() {
+		return erros;
+	}
+
+	public void addError(String fieldName, String message) {
+		erros.add(new FieldMessage(fieldName, message));
+	}
+	
+}
